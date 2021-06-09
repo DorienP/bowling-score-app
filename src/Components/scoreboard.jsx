9070074ac4c078e250c14dbe1fcb9d071cdefa33
Dorien Pine <dorien.pine@gmail.com>
2021-06-09 16:03:58 -0700

    added frame numbers

--- src/Components/scoreboard.jsx
+++ src/Components/scoreboard.jsx
@@ -4,52 +4,52 @@ export default function ScoreBoard() {
         <div className="scoreBoard">
             <div className="frame 1">
                 <div className="frame-number">
-                    <h1>1</h1>
+                    <h2 className="number">1</h2>
                 </div>
             </div>
             <div className="frame 2">
             <div className="frame-number">
-                    <h1>2</h1>
+                    <h2 className="number">2</h2>
                 </div>
             </div>
             <div className="frame 3">
             <div className="frame-number">
-                    <h1>3</h1>
+                    <h2 className="number">3</h2>
                 </div>
             </div>
             <div className="frame 4">
             <div className="frame-number">
-                    <h1>4</h1>
+                    <h2 className="number">4</h2>
                 </div>
             </div>
             <div className="frame 5">
             <div className="frame-number">
-                    <h1>5</h1>
+                    <h2 className="number">5</h2>
                 </div>
             </div>
             <div className="frame 6">
             <div className="frame-number">
-                    <h1>6</h1>
+                    <h2 className="number">6</h2>
                 </div>
             </div>
             <div className="frame 7">
             <div className="frame-number">
-                    <h1>7</h1>
+                    <h2 className="number">7</h2>
                 </div>
             </div>
             <div className="frame 8">
             <div className="frame-number">
-                    <h1>8</h1>
+                    <h2 className="number">8</h2>
                 </div>
             </div>
             <div className="frame 9">
             <div className="frame-number">
-                    <h1>9</h1>
+                    <h2 className="number">9</h2>
                 </div>
             </div>
             <div className="frame 10">
             <div className="frame-number">
-                    <h1>10</h1>
+                    <h2 className="number">10</h2>
                 </div>
             </div>
         </div>
