7be686874ffbbcdde164c186ce77abf8a4aa2610
Dorien Pine <dorien.pine@gmail.com>
2021-06-09 15:31:19 -0700

    first commit

---
+++ src/Components/scoreboard.jsx
@@ -0,0 +1,57 @@
+import './scoreboard.css';
+export default function ScoreBoard() {
+    return (
+        <div className="scoreBoard">
+            <div className="frame 1">
+                <div className="frame-number">
+                    <h1>1</h1>
+                </div>
+            </div>
+            <div className="frame 2">
+            <div className="frame-number">
+                    <h1>2</h1>
+                </div>
+            </div>
+            <div className="frame 3">
+            <div className="frame-number">
+                    <h1>3</h1>
+                </div>
+            </div>
+            <div className="frame 4">
+            <div className="frame-number">
+                    <h1>4</h1>
+                </div>
+            </div>
+            <div className="frame 5">
+            <div className="frame-number">
+                    <h1>5</h1>
+                </div>
+            </div>
+            <div className="frame 6">
+            <div className="frame-number">
+                    <h1>6</h1>
+                </div>
+            </div>
+            <div className="frame 7">
+            <div className="frame-number">
+                    <h1>7</h1>
+                </div>
+            </div>
+            <div className="frame 8">
+            <div className="frame-number">
+                    <h1>8</h1>
+                </div>
+            </div>
+            <div className="frame 9">
+            <div className="frame-number">
+                    <h1>9</h1>
+                </div>
+            </div>
+            <div className="frame 10">
+            <div className="frame-number">
+                    <h1>10</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
